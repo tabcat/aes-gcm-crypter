ede9e11a1cbd1b4c69f04a45c89b6e60cfff9e17
anders <andersb@tuta.io>
2020-07-24 00:19:40 -0500

    handle undefined .algorithm

--- src/util.js
+++ src/util.js
@@ -4,6 +4,7 @@
 exports.validKey = (cryptoKey) =>
   cryptoKey &&
   cryptoKey.type === 'secret' &&
+  cryptoKey.algorithm &&
   cryptoKey.algorithm.name === 'AES-GCM' &&
   cryptoKey.usages.includes('encrypt') &&
   cryptoKey.usages.includes('decrypt')
